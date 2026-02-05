320b5f10de5a539c067fc7b7580d9c74ebbe6528
Ngô Văn Phúc <bimbimchanel678@gmail.com>
2026-02-06 03:07:59 +0700

    Update server.js

--- kiu-casino/server.js
+++ kiu-casino/server.js
@@ -80,11 +80,22 @@ app.get('/auth/discord/callback', async (req, res) => {
     const { code } = req.query;
     if (!code) return res.send('Lỗi: Không có code!');
     try {
+        const headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'User-Agent': 'DiscordBot (https://g18game.onrender.com, 1.0.0)'
+        };
+
         const tRes = await axios.post('https://discord.com/api/oauth2/token', new URLSearchParams({
             client_id: process.env.DISCORD_CLIENT_ID, client_secret: process.env.DISCORD_CLIENT_SECRET,
             code, grant_type: 'authorization_code', redirect_uri: process.env.DISCORD_REDIRECT_URI, scope: 'identify'
-        }));
-        const uRes = await axios.get('https://discord.com/api/users/@me', { headers: { Authorization: `Bearer ${tRes.data.access_token}` } });
+        }), { headers });
+
+        const uRes = await axios.get('https://discord.com/api/users/@me', {
+            headers: {
+                Authorization: `Bearer ${tRes.data.access_token}`,
+                ...headers
+            }
+        });
         const { id, username, avatar } = uRes.data;
 
         try {
